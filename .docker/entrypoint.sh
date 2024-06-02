e2dbc2e8a7b4fdb96452688c8a4592907fec3e20
elverion <elverion@users.noreply.github.com>
2024-06-02 15:31:10 -0300

    Docker now image includes php.ini
    Copy the php.ini instead of relying on volume-mounting, to make things more generic.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Migrate DB before starting the main process
+echo "Migrating database"
 php artisan migrate --force
 
 # Open the floodgates for incoming traffic
+echo "Application is now starting up"
 php-fpm
